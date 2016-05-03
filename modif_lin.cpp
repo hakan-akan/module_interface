21faf1521b28cafb13e34559b0fdd2b1380a3fbc
hakan <h_akan@hotmail.com>
2016-05-03 16:21:07 -0400

    Changes...

--- modif_lin.cpp
+++ modif_lin.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <unistd.h> // getuid
 
 using namespace std;
 
+bool verify_root()
+{
+	if (getuid()) {
+		cout << "You must be root to do this!\n";
+		return false;
+	}
+	return true; 	
+}
 bool query_module(const std::string &m)
 {
+	if (!verify_root()) return false; 
+		
 	fstream f("/proc/modules");
-	
+	if (!f.is_open())
+		cout << "failed opening /proc/modules\n";
+		   
 	string line;
+	string name;
+	
+	size_t size, refs;
+
 	while (getline(f, line)) {
 		stringstream ssf(line);
-		unsigned long size, refs;
-		string name;
-		
+		// Parse a line
 		ssf >> name >> size >> refs;
-		
-		cout << "name " << name << endl; 
+		// match !!!
+		if(m == name) return true;
 	}
+	
+	return false;
 }
 
 bool remove_module(const std::string &m)
@@ -28,4 +45,12 @@ bool remove_module(const std::string &m)
 bool add_module(const std::string &m)
 {
 	
+}
+
+int main()
+{
+	if (query_module("r8169"))
+		cout << "found \n";
+	else
+		cout << "not found \n";	
 }
