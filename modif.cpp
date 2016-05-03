379be59b40f604f797003095c5c2e108324b1644
hakan <h_akan@hotmail.com>
2016-05-03 19:14:27 -0400

    changes

--- modif.cpp
+++ modif.cpp
@@ -1,1 +1,20 @@
+#if 
+#include <modif_lin.cpp>
+#else
+#include <modif_lin.cpp>
+#endif
 
+bool query_module(const string &m)
+{
+
+}
+
+bool remove_module(const string &m)
+{
+
+}
+
+bool add_module(const string &m)
+{
+
+}	
