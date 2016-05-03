c54014e887a20fa1922e0fb764dc980a4fd03079
hakan <h_akan@hotmail.com>
2016-05-03 18:54:30 -0400

    Changes...

--- modif_lin.cpp
+++ modif_lin.cpp
@@ -2,32 +2,31 @@
 #include <sstream>
 #include <fstream>
 #include <unistd.h> // getuid
+#include <stdexcept>
+#include <stdlib.h>
 
 using namespace std;
 
 bool verify_root()
 {
 	if (getuid()) {
-		cout << "You must be root to do this!\n";
+	//	cout << "You must be root to do this!\n";
 		return false;
 	}
 	return true; 	
 }
-bool query_module(const std::string &m)
+
+bool query_module(const string &m)
 {
-	if (!verify_root()) return false; 
-		
+	if (!verify_root()) return false;
 	fstream f("/proc/modules");
 	if (!f.is_open()) {
 		cout << "failed opening /proc/modules\n";
 		return false;
-	}
-		   
+	}	
 	string line;
 	string name;
-	
 	size_t size, refs;
-
 	while (getline(f, line)) {
 		stringstream ssf(line);
 		// Parse a line
@@ -35,24 +34,40 @@ bool query_module(const std::string &m)
 		// match !!!
 		if(m == name) return true;
 	}
-	
 	return false;
 }
 
-bool remove_module(const std::string &m)
+bool remove_module(const string &m)
 {
-	
+	if (!verify_root()) return false;
+	string cmd = "/sbin/rmmod " + m;
+	int ret = system (cmd.c_str());
+	return (ret == 0);	
 }
 
-bool add_module(const std::string &m)
+bool add_module(const string &m)
 {
-	
-}
-
+	if (!verify_root()) return false;
+	string cmd = "/sbin/modprobe " + m;
+	int ret = system (cmd.c_str());
+	return (ret == 0);	
+}	
+#if TEST
 int main()
 {
 	if (query_module("r8169"))
 		cout << "found \n";
 	else
-		cout << "not found \n";	
+		 cout << "not found \n";
+	
+	getchar();
+	
+	if (remove_module("r8169"))
+		cout << "removed \n";
+		
+	getchar();
+
+	if (add_module("r8169"))
+		cout << "added \n";
 }
+#endif
